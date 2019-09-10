bac8dab5afcb9d2fe0c8e623cfa61845320b8f43
LypkaB <lpk.bdn@gmail.com>
2019-09-10 17:31:36 +0300

    Added plugin «gulp-file-include» & new file structure in the project

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,23 @@ var notify = require('gulp-notify');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
+var fileinclude = require('gulp-file-include');
 
-gulp.task('server', ['styles'], function() {
+gulp.task('server', ['styles', 'html'], function() {
 	
 	browserSync.init({
 		server: { baseDir: '/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/'}
 	});
 
-	// watch('./app/**/*.html', browserSync.stream());
-	// watch('./app/**/*.js', browserSync.reload());
-	// watch('./app/img/*.*', browserSync.reload());
-
-
     watch(['/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/**/*.html', '/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/**/*.js', '/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/img/*.*']).on('change', browserSync.reload);
-
 
 	watch('/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/less/**/*.less', function(){
 		gulp.start('styles');
 	});
 
+    watch('/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/html/**/*.html', function(){
+        gulp.start('html');
+    });
 });
 
 gulp.task('styles', function() {
@@ -47,6 +45,23 @@ gulp.task('styles', function() {
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/css'))
 	.pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+    return gulp.src('/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/html/*.html')
+        .pipe(plumber({
+            errorHandler: notify.onError(function(err){
+                return {
+                    title: 'HTML include',
+                    sound: false,
+                    message: err.message
+                }
+            })
+        }))
+        .pipe(fileinclude({
+			prefix: '@@'
+		}))
+        .pipe(gulp.dest('/Users/l.b./HTML & CSS/1. Training/SurfingClub.com/'))
 });
 
 gulp.task('default', ['server']);
